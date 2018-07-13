0fac1117de47c608219959dc1122f78d6b63034a
carcamo <carcamo.ben@gmail.com>
2018-07-13 15:42:40 -0400

    New Day

---
+++ hello/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from .models import *
+# Create the form class.
+class SOLICITUDESForm(ModelForm):
+	class Meta:
+		model = SOLICITUDES
+		fields = ['Fecha_Vigencia', 'Version', 'Estado', 'Centro_de_Costo',
+		'Documento','Solicitado_por','Fecha_Solicitud','Fecha_Requerida'
+		,'CC','Sol','Item','Descripcion','Cant','Unid','Cert','AoD' ]
+
+
