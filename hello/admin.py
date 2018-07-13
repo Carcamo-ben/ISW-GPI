f17dfad563510b4da8c7996c2a9eb23dc144bc62
carcamo <carcamo.ben@gmail.com>
2018-07-13 13:14:58 -0400

    New Day

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(SOLICITUDES)
+admin.site.register(BODEGUEROS)
+admin.site.register(BODEGAS)
+admin.site.register(MATERIALES)
+admin.site.register(PROVEEDORES)
