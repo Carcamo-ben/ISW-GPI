480cd804c29b4253b2e62fea25b244b0c72bb386
carcamo <carcamo.ben@gmail.com>
2018-08-26 18:41:49 -0300

    New commit

--- hello/models.py
+++ hello/models.py
@@ -5,9 +5,9 @@ class SOLICITUDES(models.Model):
 	Fecha_Vigencia = models.DateField()
 	Version = models.IntegerField()
 	Estado = models.IntegerField()
-	Centro_de_Costo = models.CharField(max_length=50)
-	Documento = models.CharField(max_length=10)
-	Solicitado_por = models.CharField(max_length=50)
+	Centro_de_Costo = models.CharField(max_length=50,default=None)
+	Documento = models.CharField(max_length=10,default=None)
+	Solicitado_por = models.CharField(max_length=50,default=None)
 	Fecha_Solicitud = models.DateField()
 	Fecha_Requerida = models.DateField()
 
@@ -18,26 +18,26 @@ class Solicitud_helper(models.Model):
 	Item = models.IntegerField(default=None)
 	Descripcion = models.CharField(max_length=100,default=None)
 	Cant = models.IntegerField(default=None)
-	Unid = models.CharField(default=None)
+	Unid = models.CharField(max_length=10,default=None)
 	Cert = models.CharField(max_length=10,default=None)
 	AoD = models.CharField(max_length=20,default=None)
 
 class BODEGUEROS(models.Model):
-    Nombre = models.CharField(max_length=50)
+    Nombre = models.CharField(max_length=50,default=None)
 
 class BODEGAS(models.Model):
-    Ubicacion = models.CharField(max_length=50)
-    Hoja_de_Cargo = models.CharField(max_length=200)
+    Ubicacion = models.CharField(max_length=50,default=None)
+    Hoja_de_Cargo = models.CharField(max_length=200,default=None)
 
 class MATERIALES(models.Model):
-    Nombre = models.CharField(max_length=50)
+    Nombre = models.CharField(max_length=50,default=None)
     Cantidad = models.IntegerField()
 
 class PROVEEDORES(models.Model):
-    Nombre = models.CharField(max_length=50)
-    Direccion=models.CharField(max_length=150)
-    Rut=models.CharField(max_length=50)
-    Banco=models.CharField(max_length=50)
+    Nombre = models.CharField(max_length=50,default=None)
+    Direccion=models.CharField(max_length=150,default=None)
+    Rut=models.CharField(max_length=50,default=None)
+    Banco=models.CharField(max_length=50,default=None)
     Material1=models.CharField(max_length=100,default=None)
     Material2=models.CharField(max_length=100,default=None)
     Material3=models.CharField(max_length=100,default=None)
